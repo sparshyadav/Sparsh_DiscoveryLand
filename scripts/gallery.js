213ef9dcf81231b9b34dc361297a420fe8ffff93
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2025-02-03 14:58:07 +0530

    [Sparsh] Add: Experience Page Created

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -116,16 +116,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-window.addEventListener("scroll", function () {
-    let header = document.querySelector("header");
-    let logo = document.getElementById("logo");
-
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled");
-        logo.src = "logo-dark.png"; 
-    } else {
-        header.classList.remove("scrolled");
-        logo.src = "logo-light.png"; 
-    }
-});
